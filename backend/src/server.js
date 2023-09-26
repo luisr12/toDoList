05780b4d7c286b9e5349cad3bc80056e140360f2
LuisXV <revioredodjogos@gmail.com>
2023-09-26 00:25:36 -0300

    Separando Server Router e Aplicacao

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,8 @@
-const express = require('express');
 
-const app = express();
+const app = require('./app');
+
 const PORT = 3333;  // Defina a porta desejada, por exemplo, 3333
 
-app.get('/', (req,res)=>{
-    res.send(`Raiz`);
-})
 
 app.listen(PORT, () => {
     console.log(`Api rodando na porta ${PORT}`);
